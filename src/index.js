37bc8e6652870b6140c0f55e1c7e65491dbfc479
DaryaGorbachova <vrindavaneshvari@mail.ru>
2021-08-06 16:29:41 +0300

    0-9

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function toReadable (number) {
-  
+        number = number.toString();
+        let result = '';
+        if (number.length === 1) {
+            
+            switch (number) {
+                case '0': result += 'zero';
+                    break;
+                case '1': result += 'one';
+                    break;
+                case '2': result += 'two';
+                    break;
+                case '3': result += 'three';
+                    break;
+                case '4': result += 'four';
+                    break;
+                case '5': result += 'five';
+                    break;
+                case '6': result += 'six';
+                    break;
+                case '7': result += 'seven';
+                    break;
+                case '8': result += 'eight';
+                    break;
+                case '9': result += 'nine';
+                    break;			
+                default: result = '';
+            }
+        }
+            return result;
+        
 }
